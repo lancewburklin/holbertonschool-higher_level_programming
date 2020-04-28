e759caf8089530536f12f9db0eabbd9516b87480
lancewburklin <1677@holbertonschool.com>
2020-04-28 23:59:57 +0000

    Condensed further

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-x = False
 for i in range(0, 100):
-    if i == 99:
-        x = True
     if i < 10:
         print("{:d}".format(0), end="")
     if i != 100:
-        print("{:d}{:s}{:s}".format(i, "" if x else ",", "\n" if x else " "), end="")
+        print("{:d}{:s}".format(i, ", " if i != 99 else "\n"), end ="")
