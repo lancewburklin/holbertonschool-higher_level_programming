100b74145bd3dbc79f3af9245789b5d82cd74e9b
lancewburklin <1677@holbertonschool.com>
2020-04-29 01:44:02 +0000

    Fixed pep8 issues

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(0, 100):
     if i < 10:
         print("{:d}".format(0), end="")
     if i != 100:
-        print("{:d}{:s}".format(i, ", " if i != 99 else "\n"), end ="")
+        print("{:d}{:s}".format(i, ", " if i != 99 else "\n"), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
+    a = 96
+    b = 123
     for i in str:
-        print("{}".format(chr(ord(i) - 32) if 96 < ord(i) < 123 else i), end="")
+        print("{}".format(chr(ord(i) - 32) if a < ord(i) < b else i), end="")
     print("{}".format("\n"), end="")
