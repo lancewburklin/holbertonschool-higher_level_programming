4fcae91aff81c43c3f543fe7bf3bcf703eaac516
lancewburklin <1677@holbertonschool.com>
2020-04-30 20:04:14 +0000

    Import protected

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import add_0 as func
-a = 1
-b = 2
-result = func.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+def main():
+    import add_0 as func
+    a = 1
+    b = 2
+    result = func.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    main()
