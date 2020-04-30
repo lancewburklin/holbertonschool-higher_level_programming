6a7eda545cc747d8b6cc15be1cb2cbe6136be39e
lancewburklin <1677@holbertonschool.com>
2020-04-30 19:43:26 +0000

    Prints the arguments given

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+args = sys.argv
+arglen = len(args) - 1
+count = 0
+if arglen == 0:
+    print("0 arguments.")
+elif arglen == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(arglen))
+for i in args[1:]:
+    count = count + 1
+    print("{}: {}".format(count, i))
