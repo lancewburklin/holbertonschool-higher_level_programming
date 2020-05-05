ab012482cb66a7ac6f26ec7cffa4bbaecb417c46
lancewburklin <1677@holbertonschool.com>
2020-05-05 18:48:04 +0000

    Finds an element in an array

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx > len(my_list):
+        return (None)
+    elif idx < 0:
+        return (None)
+    for i in range(len(my_list)):
+        if i == idx:
+            return (my_list[i])
+            
