866a04a1c4ec6f06660ccf085fa6a55997d48b1a
lancewburklin <1677@holbertonschool.com>
2020-05-06 04:10:37 +0000

    Fixed indexing issues

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx > len(my_list) - 1:
+    if idx > len(my_list):
         return (my_list)
     if idx < 0:
         return (my_list)
-    newe_list = []
-    for i in range(len(my_list) - 1):
+    for i in range(len(my_list)):
         if i == idx:
             my_list.remove(my_list[i])
     return (my_list)
