402480af292c3da0ee7312fdde5b9959a221bfe5
lancewburklin <1677@holbertonschool.com>
2020-05-06 00:51:10 +0000

    Changed method of removing C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        new_string = ""
+        new_string = list(my_string)
         for i in range(len(my_string)):
-            if ord(my_string[i]) != 99 and ord(my_string[i]) != 67:
-                new_string = new_string + my_string[i]
-    return (new_string)
+            if ord(my_string[i]) == 99 or ord(my_string[i]) == 67:
+                new_string.remove(my_string[i])
+        my_string = "".join(new_string)
+        return (my_string)
