ebc206282ab6164544b1700482423ecf1a4cb5d0
lancewburklin <1677@holbertonschool.com>
2020-05-06 00:28:00 +0000

    Changed return indent

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,4 +5,4 @@ def no_c(my_string):
         for i in range(len(my_string)):
             if ord(my_string[i]) != 99 and ord(my_string[i]) != 67:
                 new_string = new_string + my_string[i]
-        return (new_string)
+    return (new_string)
