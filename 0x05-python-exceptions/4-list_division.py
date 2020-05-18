73c833ee3ab16619cd15afd22f6ed27864f04ca0
lancewburklin <1677@holbertonschool.com>
2020-05-18 19:32:29 +0000

    Divides two lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    i = 0
+    my_list = []
+    while i < list_length:
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            my_list.append(result)
+        i = i + 1
+    return(my_list)
