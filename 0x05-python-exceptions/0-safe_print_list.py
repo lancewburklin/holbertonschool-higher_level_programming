04c5cea15be1bdd757b8c425845a56b87b4233dd
lancewburklin <1677@holbertonschool.com>
2020-05-18 19:05:00 +0000

    Fixed newline

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    ending = ""
     while i < x:
         try:
-            if i == x-1:
-                ending = '\n'
-            print("{}".format(my_list[i]), end=ending)
+            print("{}".format(my_list[i]), end="")
             i = i + 1
         except:
             print("")
             return(i)
+    print("")
     return(i)
