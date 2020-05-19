00cd08fdea8ed8838ef9df4ad92774810979e39f
lancewburklin <1677@holbertonschool.com>
2020-05-19 22:26:53 +0000

    Assigns private variable with self

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,9 +5,7 @@
 
 class Square:
     """This is an object. A square object."""
-
     __size = 0
 
     def __init__(self, size):
-        """This is the size of the square"""
-        Square.__size = size
+        self.__size = size
