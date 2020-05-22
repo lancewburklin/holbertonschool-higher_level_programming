18203667a9d592f49d5247a8cd082aeda7507c29
lancewburklin <1677@holbertonschool.com>
2020-05-22 21:33:55 +0000

    Fixed pep8 Issues

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+
+""" This module will say a name and that is all"""
+
+
 def say_my_name(first_name, last_name=""):
-    if isinstance(first_name, str) == False:
+    """NAME SAYER"""
+    if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
-    if isinstance(last_name, str) == False:
+    if not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
     print("My name is {} {}".format(first_name, last_name))
