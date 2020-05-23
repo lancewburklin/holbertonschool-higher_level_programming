13bc44b72de8322545982d72a60bc17a8a35e5c6
lancewburklin <1677@holbertonschool.com>
2020-05-23 04:29:38 +0000

    Added tests

--- 0x07-python-test_driven_development/main_tests/6-main.py
+++ 0x07-python-test_driven_development/main_tests/6-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 max_integer = __import__('6-max_integer').max_integer
 
+list_all = ["St", "Str", "ringring"]
 print(max_integer([1, 2, 3, 4]))
 print(max_integer([1, 3, 4, 2]))
 print(max_integer("TEST"))
+print(max_integer(list_all))
