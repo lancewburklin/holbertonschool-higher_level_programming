22e872cdaa9724256bada0178087134b4ee2807c
lancewburklin <1677@holbertonschool.com>
2020-06-02 23:40:24 +0000

    Creates a square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Creates a square based off the rectangle
+"""
+
+
+rect = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(rect):
+    """The square class"""
+
+    def __init__(self, size):
+        """Initializes the square"""
+        super().__init__(size, size)
+        bg = BaseGeometry()
+        bg.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Finds the area of the square"""
+        return self.__size ** 2
