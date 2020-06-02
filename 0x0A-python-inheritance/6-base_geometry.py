cf4436c1d50d6c38fdadfc811db29b04c0cf502d
lancewburklin <1677@holbertonschool.com>
2020-06-02 21:49:11 +0000

    Sets up area method for geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Adds area to Geometry
+"""
+
+
+class BaseGeometry:
+    """Geometry class"""
+
+    def area(self):
+        """Finds the area"""
+        raise Exception('area() is not implemented')
