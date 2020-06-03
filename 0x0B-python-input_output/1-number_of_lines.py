cfd30ecc242fcd18e535c39d2888a5dbfea2dc56
lancewburklin <1677@holbertonschool.com>
2020-06-03 16:05:31 +0000

    Counts number of lines in a file

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Finds the number of lines in a file
+"""
+
+
+def number_of_lines(filename=""):
+    line_number = 0
+    with open(filename, encoding='utf-8') as file_desc:
+        for lines in file_desc:
+            line_number += 1
+    return line_number
