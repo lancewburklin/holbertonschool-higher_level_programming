30745e549f1c9a2a1959654ae5ae942547e5d600
lancewburklin <1677@holbertonschool.com>
2020-06-03 18:21:17 +0000

    Saves JSON data to file

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Saves info to a file
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    import json
+    with open(filename, mode='w', encoding='utf-8') as a_file:
+        json.dump(my_obj, a_file)
