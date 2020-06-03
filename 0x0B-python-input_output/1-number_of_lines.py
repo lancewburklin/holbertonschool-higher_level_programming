6a41dd3356dbe6de7d1cbbad51d096fe8df79f34
lancewburklin <1677@holbertonschool.com>
2020-06-03 18:01:33 +0000

    Added more doc string

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -5,6 +5,7 @@ Finds the number of lines in a file
 
 
 def number_of_lines(filename=""):
+    """Start of function"""
     line_number = 0
     with open(filename, encoding='utf-8') as file_desc:
         for lines in file_desc:
