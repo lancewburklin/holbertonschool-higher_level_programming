e22d6d80ab2e6ab4964a233d0fe5755e7e009efa
lancewburklin <1677@holbertonschool.com>
2020-06-03 18:02:06 +0000

    Added more doc string

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -5,6 +5,7 @@ Writes to a file
 
 
 def write_file(filename="", text=""):
+    """Writing to a file"""
     with open(filename, mode='w', encoding='utf-8') as a_file:
         a_file.write(text)
     return len(text)
