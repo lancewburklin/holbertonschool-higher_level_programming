5e233a5ce7b7e0fec3add132a84b737469a90ea7
lancewburklin <1677@holbertonschool.com>
2020-06-04 03:41:19 +0000

    Added more doc string

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -5,6 +5,7 @@ Saves info to a file
 
 
 def save_to_json_file(my_obj, filename):
+    """Saving to a file"""
     import json
     with open(filename, mode='w', encoding='utf-8') as a_file:
         json.dump(my_obj, a_file)
