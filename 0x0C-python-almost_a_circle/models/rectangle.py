241539b8281587014d66b128c090f54fa826593c
lancewburklin <1677@holbertonschool.com>
2020-06-09 18:14:21 +0000

    Updated class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,42 @@ class Rectangle(Base):
     def area(self):
         """Finds the area"""
         return self.__height * self.__width
+
+    def display(self):
+        """Displays the rectangle"""
+        print("\n" * self.__y, end="")
+        for i in range(self.__height):
+            print(" " * self.__x, end="")
+            print("#" * self.__width)
+
+    def __str__(self):
+        """Returns info for the rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """Updates the rectangle"""
+        counter = 1
+        for i in args:
+            if counter == 1:
+                self.id = args[0]
+            if counter == 2:
+                self.__width = args[1]
+            if counter == 3:
+                self.__height = args[2]
+            if counter == 4:
+                self.__x = args[3]
+            if counter == 5:
+                self.__y = args[4]
+            counter += 1
+        if len(args) < 6:
+            if len(args) < 1 and 'id' in kwargs.keys():
+                self.id = kwargs['id']
+            if len(args) < 2 and 'width' in kwargs.keys():
+                self.__width = kwargs['width']
+            if len(args) < 3 and 'height' in kwargs.keys():
+                self.__height = kwargs['height']
+            if len(args) < 4 and 'x' in kwargs.keys():
+                self.__x = kwargs['x']
+            if len(args) < 5 and 'y' in kwargs.keys():
+                self.__y = kwargs['y']
