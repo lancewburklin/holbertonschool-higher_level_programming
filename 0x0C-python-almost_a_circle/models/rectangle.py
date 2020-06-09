a909782aa268cc3ff4c2771af06a2dee8b2491eb
lancewburklin <1677@holbertonschool.com>
2020-06-09 15:03:04 +0000

    Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,59 +12,65 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
 
     @property
     def width(self):
         """Gets the width"""
         return self.__width
 
+    @width.setter
+    def width(self, value):
+        """Sets width"""
+        Rectangle.int_validator(value, "width")
+        self.__width = value
+
     @property
     def height(self):
         """Gets the height"""
         return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height"""
+        Rectangle.int_validator(value, "height")
+        self.__height = value
 
     @property
     def x(self):
         """Gets x"""
         return self.__x
 
+    @x.setter
+    def x(self, value):
+        """Sets x"""
+        Rectangle.int_validator(value, "x")
+        self.__x = value
+
     @property
     def y(self):
         """gets y"""
         return self.__y
 
-    def int_validator(self, value, attr):
-        """Validates attributes"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(attr))
-        if value <= 0:
-            raise ValueError("{} must be >= 0".format(attr))
-
-    @width.setter
-    def width(self, value):
-        """Sets width"""
-        Rectangle.int_validator(value, width)
-        self.__width = value
-
-    @height.setter
-    def height(self, value):
-        """ Sets height"""
-        Rectangle.int_validator(value, height)
-        self.__height = value
-
-    @x.setter
-    def x(self, value):
-        """Sets x"""
-        Rectangle.int_validator(value, x)
-        self.__x = x
-
     @y.setter
     def y(self, value):
-        """Sets y"""
-        Rectangle.int_validator(value, y)
-        self.__y = y
+        """sets y"""
+        Rectangle.int_validator(value, "y")
+        self.__y = value
+
+    def int_validator(value, attr):
+        """Validates attributes"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(attr))
+        if value < 0 and attr in {"x", "y"}:
+            raise ValueError("{} must be >= 0".format(attr))
+        if value <= 0 and attr in {"height", "width"}:
+            raise ValueError("{} must be > 0".format(attr))
+
+    def area(self):
+        """Finds the area"""
+        return self.__height * self.__width
