481ac146022e3404dd400b90bd76af3f320ac7b0
lancewburklin <1677@holbertonschool.com>
2020-06-09 19:36:36 +0000

    Updates to class, adds update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,3 +16,37 @@ class Square(Rectangle):
         """Square info"""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.height)
+
+    @property
+    def size(self):
+        """Gets size"""
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        """Sets the size"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Updates the square"""
+        counter = 1
+        for i in args:
+            if counter == 1:
+                self.id = args[0]
+            if counter == 2:
+                self.size = args[1]
+            if counter == 3:
+                self.x = args[2]
+            if counter == 4:
+                self.y = args[3]
+            counter += 1
+        if len(args) < 5:
+            if len(args) < 1 and 'id' in kwargs.keys():
+                self.id = kwargs['id']
+            if len(args) < 2 and 'size' in kwargs.keys():
+                self.size = kwargs['size']
+            if len(args) < 3 and 'x' in kwargs.keys():
+                self.x = kwargs['x']
+            if len(args) < 4 and 'y' in kwargs.keys():
+                self.y = kwargs['y']
