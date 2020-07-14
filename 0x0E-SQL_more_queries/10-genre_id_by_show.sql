7727f868f5dfab8ca22583308aad804065989ea7
lancewburklin <1677@holbertonschool.com>
2020-07-14 21:26:11 +0000

    Orders shows and displays genres

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- Find the show and genre
+SELECT tv_shows.title, tv_show_genres.genre_id from tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
