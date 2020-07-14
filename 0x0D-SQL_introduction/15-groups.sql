f71f8d42126511b241a16fddab80a834b8432823
lancewburklin <1677@holbertonschool.com>
2020-07-14 03:35:56 +0000

    Fixed to order in descent

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Finding duplicates
-SELECT score, COUNT(score) number FROM second_table GROUP BY score HAVING COUNT(score) > 1; 
+SELECT score, COUNT(score) number FROM second_table GROUP BY score HAVING COUNT(score) > 1 ORDER BY score DESC; 
