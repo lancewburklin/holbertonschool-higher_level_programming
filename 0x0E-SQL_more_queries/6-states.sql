894d5e811cb354dbdcf8a66ba318f25858a4f86d
lancewburklin <1677@holbertonschool.com>
2020-07-14 20:03:13 +0000

    Creates a new database and table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- Creates a new database and state
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
