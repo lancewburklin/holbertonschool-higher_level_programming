67825fff8a2f143d2cb26dc983f828106e569e05
lancewburklin <1677@holbertonschool.com>
2020-08-07 23:56:43 +0000

    Added docstring and pep8 compliant

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""
+Start link class to table in database
 """
 import sys
 from model_state import Base, State
@@ -7,13 +8,15 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    """ Gets all the states """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind = engine)
-session = Session()
-result = session.query(State).all()
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).all()
 
-for row in result:
-    print("{}: {}".format(row.id, row.name))
+    for row in result:
+        print("{}: {}".format(row.id, row.name))
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""
+Start link class to table in database
 """
 import sys
 from model_state import Base, State
@@ -7,13 +8,17 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    """ Gets the first state """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     from sqlalchemy.orm import sessionmaker
-    Session = sessionmaker(bind = engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
     result = session.query(State).first()
 
-    if (result != None):
+    if (result):
         print("{}: {}".format(result.id, result.name))
+    else:
+        print("Nothing")
