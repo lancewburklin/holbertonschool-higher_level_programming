93bd1470f8dfbed4495f10f0b7fa1debe1f65b4b
lancewburklin <1677@holbertonschool.com>
2020-08-08 01:38:33 +0000

    Changes the state name by id

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Start link class to table in database
+"""
+
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    """ Updates state with ID 2 """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(State).filter(State.id == 2).update({State.name:"New Mexico"})
+    session.commit()
