60f46c08374eb20b75e425a538bdf7d10d843d6b
lancewburklin <1677@holbertonschool.com>
2020-08-18 22:06:54 +0000

    Gets info from a specific ip

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Tracks down the body of the catch_me redirect
-curl -sL "$1" -X PUT -d "user_id=98" -H "Origin: HolbertonSchool"
+curl -sL 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "Origin: HolbertonSchool"
