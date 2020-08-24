37e974aa95bbe5aa5053f3813b590bb7dc60fa1b
lancewburklin <1677@holbertonschool.com>
2020-08-24 19:28:27 +0000

    Gets some JSON info

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Send a letter
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+    if len(sys.argv) != 1:
+        test = str(sys.argv[1])
+    else:
+        test = ""
+    thing = requests.post('http://0.0.0.0:5000/search_user', data={'q': test})
+    try:
+        if len(thing.json()) == 0:
+            print("No result")
+        else:
+            derulo = thing.json()
+            print("[{}] [{}]".format(derulo.get("id"), derulo.get("name")))
+    except:
+        print("Not a valid JSON")
