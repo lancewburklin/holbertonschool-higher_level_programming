50828ee640d9e4971309b193e24a9a9b6a335069
lancewburklin <1677@holbertonschool.com>
2020-09-15 17:37:14 +0000

    Fixed semistandard errors

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-if (typeof(process.argv[2]) === 'undefined'){
-  const par1 = 'undefined';
+if (typeof (process.argv[2]) === 'undefined') {
   console.log('undefined is undefined');
 } else {
   const par1 = process.argv[2];
-  if (typeof(process.argv[3]) === 'undefined'){
-  console.log(par1 + ' is undefined');
-} else {
-  console.log(par1 + ' is ' + process.argv[3]);
-}
+  if (typeof (process.argv[3]) === 'undefined') {
+    console.log(par1 + ' is undefined');
+  } else {
+    console.log(par1 + ' is ' + process.argv[3]);
+  }
 }
