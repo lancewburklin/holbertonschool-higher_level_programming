c77d5d27dab3f935dfda5fbab1377de1c9942013
lancewburklin <1677@holbertonschool.com>
2020-09-15 20:16:14 +0000

    Reworked code, fixed bugs

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,33 @@
 #!/usr/bin/node
-if (process.argv.length < 4) {
-  console.log(0);
-} else {
-  let x = parseInt(process.argv[2]);
-  let y = parseInt(process.argv[2]);
-  for (let i = 2; i < process.argv.length; i++) {
-    if (x < parseInt(process.argv[i])) {
+function findLargest (index) {
+  const args = process.argv;
+  let x = parseInt(args[index]);
+  let y = parseInt(args[index]);
+  for (let i = index; i < args.length; i++) {
+    if (x < parseInt(args[i])) {
       y = x;
-      x = parseInt(process.argv[i]);
+      x = parseInt(args[i]);
     }
-    if (i + 1 === process.argv.length) {
-      console.log(y);
+    if (i + 1 === args.length) {
+      if (x !== y && index === 2) {
+        return (y);
+      } else {
+        if (index === 2) {
+          return (findLargest(3));
+        }
+      }
     }
   }
+  return (x);
+}
+
+const args = process.argv;
+if (args.length < 4) {
+  console.log(0);
+} else {
+  if (parseInt(args[2]) === parseInt(args[args.length - 1])) {
+    process.argv[args.length - 1] = -99999999999999999999;
+  }
+  const res = findLargest(2);
+  console.log(res);
 }
