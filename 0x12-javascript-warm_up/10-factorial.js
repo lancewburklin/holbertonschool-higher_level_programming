edecc5f067939c89ed9321d1578c929de13568c7
lancewburklin <1677@holbertonschool.com>
2020-09-15 20:33:41 +0000

    Refactored

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 function findFactorial (mult, num) {
-  if (mult !== 0) {
-    findFactorial(mult - 1, num * mult);
-  } else {
-    console.log(num);
-  }
+  return (findFactorial(mult - 1, num * mult));
 }
 if (isNaN(parseInt(process.argv[2]))) {
   console.log(1);
 } else {
-  findFactorial(parseInt(process.argv[2]) - 1, parseInt(process.argv[2]));
+  const args = process.argv;
+  console.log(findFactorial(parseInt(args[2]) - 1, parseInt(args[2])));
 }
