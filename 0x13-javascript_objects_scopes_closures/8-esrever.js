ac3ec7bdd05fad80a212cdfc26a315f704223221
lancewburklin <1677@holbertonschool.com>
2020-09-21 19:13:12 +0000

    Completely refactored

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  class Newlist {
-    constructor () {
-      this.Thelist = [];
-    }
-  }
   let idx = list.length - 1;
   let n = 0;
-  for (let aList = new Newlist(); idx >= 0; idx--, n++) {
-    aList.Thelist[n] = list[idx];
+  for (let aList = []; idx >= 0; idx--, n++) {
+    aList[n] = list[idx];
     if (idx === 0) {
-      return (aList.Thelist);
+      return (aList);
     }
   }
 };
