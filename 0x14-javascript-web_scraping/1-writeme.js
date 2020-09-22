c5bcbced4bb6428cc45901dfeffa29d7ab6a6c5b
lancewburklin <1677@holbertonschool.com>
2020-09-22 17:00:26 +0000

    Writes to a file in JS

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const info = process.argv[3] + '\n';
+
+fs.writeFile(file, info, function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
