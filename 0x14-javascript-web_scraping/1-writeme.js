eeacc98a71b14ac201ee5117ca3ba3ecc13dce96
lancewburklin <1677@holbertonschool.com>
2020-09-22 17:04:10 +0000

    Removed extra newline

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const file = process.argv[2];
-const info = process.argv[3] + '\n';
+const info = process.argv[3];
 
 fs.writeFile(file, info, function (err) {
   if (err) {
