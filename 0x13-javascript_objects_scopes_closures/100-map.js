5b198dff451213a1e17144cdd69aa9f9ecd131c0
lancewburklin <1677@holbertonschool.com>
2020-09-23 14:44:08 +0000

    Using map to alter an array

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const list = require('./100-data').list;
+
+const newList = list.map(function (element, index,  obj) {
+  return element * index;
+});
+
+console.log(list);
+console.log(newList);
