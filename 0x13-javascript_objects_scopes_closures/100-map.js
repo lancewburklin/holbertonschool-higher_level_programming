2aa2d778755031b97016808c3c82fe418d225747
lancewburklin <1677@holbertonschool.com>
2020-09-23 14:45:17 +0000

    Fixed semistandard issues

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-const newList = list.map(function (element, index,  obj) {
+const newList = list.map(function (element, index, obj) {
   return element * index;
 });
 
