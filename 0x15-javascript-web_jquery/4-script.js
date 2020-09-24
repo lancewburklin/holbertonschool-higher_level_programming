dcaae3042916f8d5776a13f860d845ca1d4787d2
lancewburklin <1677@holbertonschool.com>
2020-09-24 21:16:04 +0000

    Changes text color when clicked

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const $ = window.$;
+$('#toggle_header').click(function () {
+  if ($('header').hasClass('green')) {
+    $('header').removeClass('green');
+    $('header').addClass('red');
+  } else {
+    $('header').removeClass('red');
+    $('header').addClass('green');
+  }
+});
